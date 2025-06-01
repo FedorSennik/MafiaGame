057d8b429a1bf130ce70c8cd42fd3e8b367617ee
FedorSennik <fedorsennik@gmail.com>
2025-06-01 20:43:45 +0300

    + допрацював меню кнопок

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+
+public class KeybindManager : MonoBehaviour
+{
+   
+    public Dictionary<string, KeyCode> keyBindings = new Dictionary<string, KeyCode>();
+    public string currentBindingKey;
+    public bool isRebinding = false;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+        if (isRebinding)
+        {
+            ChangeKeyBinding();
+        }
+    }
+
+    public void Init()
+    {
+        keyBindings.Add("Reload", KeyCode.R);
+        keyBindings.Add("Shoot", KeyCode.Mouse0);
+        keyBindings.Add("Jump", KeyCode.Space);
+        keyBindings.Add("Sprint", KeyCode.LeftShift);
+    }
+
+    public void Rebind(string bindingKey)
+    {
+        currentBindingKey = bindingKey;
+        isRebinding = true;
+    }
+
+    public void ChangeKeyBinding()
+    {
+        foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(key))
+            {
+                keyBindings[currentBindingKey] = key;
+                isRebinding = false;
+                break;
+
+            }
+        }
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    public UIManager UIManager;
     public float speed = 5f;
-    public float jumpForce = 7f;
+    public float jumpForce = 4f;
     public float mouseSensitivity = 2f;
+
+    private KeyCode _forwardKey = KeyCode.W;
+    private KeyCode _backwardKey = KeyCode.S;
+    private KeyCode _leftKey = KeyCode.A;
+    private KeyCode _rightKey = KeyCode.D;
+    private KeyCode _jumpKey = KeyCode.Space;
+
+
+    public KeyCode ForwardKey
+    {
+        get { return _forwardKey; }
+        set { _forwardKey = value; }
+    }
+
+    public KeyCode BackwardKey
+    {
+        get { return _backwardKey; }
+        set { _backwardKey = value; }
+    }
+
+    public KeyCode LeftKey
+    {
+        get { return _leftKey; }
+        set { _leftKey = value; }
+    }
+
+    public KeyCode RightKey
+    {
+        get { return _rightKey; }
+        set { _rightKey = value; }
+    }
+
+    public KeyCode JumpKey
+    {
+        get { return _jumpKey; }
+        set { _jumpKey = value; }
+    }
+
+
     private Rigidbody rb;
     private bool isGrounded;
     private Transform cameraTransform;
@@ -16,20 +58,20 @@ public class PlayerController : MonoBehaviour
         cameraTransform = Camera.main.transform;
         Cursor.lockState = CursorLockMode.Locked;
 
-        // Заблокувати нахил персонажа (щоб не перекидався)
+        // Заблокувати нахил персонажа
         rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
 
     void Update()
     {
-        // Рух персонажа
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
+        // Рух персонажа зі змінними кнопками введення
+        float moveX = (Input.GetKey(_leftKey) ? -1 : 0) + (Input.GetKey(_rightKey) ? 1 : 0);
+        float moveZ = (Input.GetKey(_forwardKey) ? 1 : 0) + (Input.GetKey(_backwardKey) ? -1 : 0);
         Vector3 movement = transform.right * moveX + transform.forward * moveZ;
         rb.velocity = new Vector3(movement.x * speed, rb.velocity.y, movement.z * speed);
 
         // Стрибок
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(_jumpKey) && isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
@@ -39,10 +81,18 @@ public class PlayerController : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Обмежуємо поворот камери
-
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
+
+        if (UIManager.AllUIIsClosed)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
     void OnCollisionStay(Collision collision)
